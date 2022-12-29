3c3d82bbf6118816d2fe41585bde2676c98d23fc
Emaani-gh <abdulsalamseidu@protonmail.com>
2022-12-29 21:24:21 +0200

    fixed config.js

--- config/config.js
+++ config/config.js
@@ -20,8 +20,8 @@ module.exports = {
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASS,
-    "database": DB_NAME,
-    "host": DB_HOST ,
-    "dialect": DB_DIALET
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST ,
+    "dialect": process.env.DB_DIALET
   }
 }
